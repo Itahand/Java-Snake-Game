a570a3de5e822bf56b42ab2bbc405eb3a92448be
Itahand Naizir <itahand.naizir@gmail.com>
2022-09-29 11:26:53 -0500

    Starting the game constructor.

--- src/main/java/com/example/GamePanel.java
+++ src/main/java/com/example/GamePanel.java
@@ -1,12 +1,67 @@
 package com.example;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
 import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGTH = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_HEIGTH*SCREEN_WIDTH)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 5;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    GamePanel () {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_HEIGTH, SCREEN_WIDTH));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdaptor());
+        startGame();
+    }
+    public void startGame() {
+
+    }
+    public void printComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+    @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+    public class MyKeyAdaptor extends KeyAdapter {
+        @Override
+        public void keyPressed(keyEvent e) {
+
+        }
     }
 }
--- src/main/java/com/example/SnakeGame.java
+++ src/main/java/com/example/SnakeGame.java
@@ -1,8 +1,9 @@
 package com.example;
 
 public class SnakeGame {
-  public static void main( String[] args )
-  {
-      System.out.println( "Hello World!!!!!" );
+  public static void main( String[] args ) {
+
+    new GameFrame();
+
   }
 }
